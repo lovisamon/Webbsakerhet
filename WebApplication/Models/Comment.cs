e4c7bd7160d5615e0ccb613f9999b1688765d1b3
Lovisa Montelius <lovisamon@gmail.com>
2022-01-06 20:40:35 +0100

    Add project files.

---
+++ WebApplication/Models/Comment.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace WebApplication.Models
+{
+    public class Comment
+    {
+        public Guid Id { get; set; }
+        public DateTime TimeStamp { get; set; }
+        public string Content { get; set; }
+    }
+}
